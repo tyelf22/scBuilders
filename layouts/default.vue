2ae9fb0d5a9dd1cd54c3cdee387ae7e8202e0dce
tyelf22 <tyelf22@hotmail.com>
2020-07-02 12:39:48 -0600

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <v-toolbar>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
+    
+    <nuxt />
+
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<style>
+  .mainColor{
+    color: yellow;
+  }
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <v-parallax
+      dark
+      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    >
+      <v-row
+        align="center"
+        justify="center"
+      >
+        <v-col class="text-center" cols="12">
+          <h1 class="display-1 font-weight-thin mb-3 mainColor">SC Builders, inc</h1>
+          <h4 class="subheading">Trusted for over 23 years</h4>
+        </v-col>
+      </v-row>
+    </v-parallax>
+    <v-container class="grey lighten-5">
+      <v-row no-gutters>
+        <v-col
+          cols="12"
+          sm="12"
+          lg="4"
+        >
+          <v-card
+            class="pa-2"
+            outlined
+            tile
+          >
+            One of three columns
+          </v-card>
+        </v-col>
+        <v-col
+          cols="12"
+          sm="12"
+          lg="4"
+        >
+          <v-card
+            class="pa-2"
+            outlined
+            tile
+          >
+            Two of three columns
+          </v-card>
+        </v-col>
+        <v-col
+          cols="12"
+          sm="12"
+          lg="4"
+        >
+          <v-card
+            class="pa-2"
+            outlined
+            tile
+          >
+            Three of three columns
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-container class="grey lighten-5">
+      <v-row justify="center">
+        <v-col
+          cols="12"
+          sm="12"
+          lg="4"
+        >
+          <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          One of three columns
+        </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
